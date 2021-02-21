849a9c5c609bf2a035682ae1e478cfc9ad21df32
Adam Zdzeshchyts <adamchic2@tut.by>
2021-02-21 16:19:52 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checking = 0,
+  checkingForRepeating = 0,
+    closingShouldBe = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        //если для текущей скобки из последовательности открывающая и закрывающая одинаковы
+      if (
+        str[i] === bracketsConfig[j][0] &&
+        bracketsConfig[j][0] === bracketsConfig[j][1]
+      ) {
+        checkingForRepeating++;
+        //если повторяющиеся скобки нечётного количества
+        //if (checkingForRepeating%2 === 1) closingShouldBe.push(bracketsConfig[j][1]);
+        
+        if (str[i] !== closingShouldBe[closingShouldBe.length - 1]) 
+        {
+            if (closingShouldBe.includes(str[i])) return false;
+            closingShouldBe.push(bracketsConfig[j][1]);
+        }
+        else closingShouldBe.pop(bracketsConfig[j][1]);
+      }
+      //если текущая скобка из последовательности - открывающая
+      else if (str[i] === bracketsConfig[j][0]) {
+        checking++;
+        closingShouldBe.push(bracketsConfig[j][1]);
+        //console.log(closingShouldBe);
+      }
+      //если текущая скобка из последовательности - закрывающая
+      else if (str[i] === bracketsConfig[j][1]) {
+        if (str[i] !== closingShouldBe[closingShouldBe.length - 1])
+          return false; //если закрывающая скобка не та, что требуется
+        closingShouldBe.pop();
+        checking--;
+      }
+    }
+    if (checking < 0) return false; //если где-то скобка закрывающая оказалась раньше открывающей
+  }
+  if (checkingForRepeating%2 === 1) return false;//если повторяющиеся скобки нечётного числа (т.е. нет закрывающей)
+  if (checking !== 0) return false; //если число скобок открывающих не совпадает с числом закрывающих
+  return true;
 }
